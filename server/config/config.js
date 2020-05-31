cb9a9fab90f224ab3fbb7f64fe37f2a3481e2f11
Gino Su <ginosuarellan@gmail.com>
2020-05-31 06:32:12 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://pekim:BmcqNdmEwiM7511O@cluster0-9ohmd.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;

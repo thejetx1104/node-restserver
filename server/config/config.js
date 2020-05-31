e6bea7843604c36d9cc66485fb2ab2d272bc2860
Gino Su <ginosuarellan@gmail.com>
2020-05-31 05:26:02 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,18 @@
+//Puerto
+
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Base de datos
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://pekim:BmcqNdmEwiM7511O@cluster0-9ohmd.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
